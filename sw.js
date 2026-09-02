428749003e03c7b25049863d4a1d4fb3ec0c73bd
78jtun-alt <78jtun@gmail.com>
2026-09-02 20:47:25 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
-self.addEventListener('fetch',(e)=>{
-  
-})
+const CACHE_NAME = 'grammar-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './style.css', './script.js', './manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
